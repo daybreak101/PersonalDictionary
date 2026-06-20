b8296c97f00f0c9ab3fed1eaca257a443c5cae4e
Ramiro F <ramirofernandez244@gmail.com>
2026-06-19 18:51:55 -0700

    - Updated icon

--- app.config.js
+++ app.config.js
@@ -6,7 +6,11 @@ export default {
     slug: "PersonalDictionary",
     version: "1.0.0",
     android: {
-      package: "com.anonymous.PersonalDictionary",
+      package: "com.rferna25.PersonalDictionary",
+      adaptiveIcon: {
+        foregroundImage: "./assets/personal-dictionary-high-resolution-logo.png",
+        backgroundColor: "#ffffff",
+      },
     },
     plugins: ["expo-asset", "expo-audio"],
     extra: {
@@ -14,5 +18,6 @@ export default {
         projectId: "2e0861a2-6192-4e27-800d-e3d8b90ddeb8",
       },
     },
+    icon: "./assets/personal-dictionary-high-resolution-logo.png",
   },
 };
