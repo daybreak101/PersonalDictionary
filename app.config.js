f88bfcd1f3b0d3c650c3d4633ec4266e2b9951d3
Ramiro F <ramirofernandez244@gmail.com>
2026-06-19 18:25:48 -0700

    - EAS config

--- app.config.js
+++ app.config.js
@@ -9,5 +9,10 @@ export default {
       package: "com.anonymous.PersonalDictionary",
     },
     plugins: ["expo-asset", "expo-audio"],
+    extra: {
+      eas: {
+        projectId: "2e0861a2-6192-4e27-800d-e3d8b90ddeb8",
+      },
+    },
   },
 };
